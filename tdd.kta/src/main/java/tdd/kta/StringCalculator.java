0e7d654506edff5086d2aad2a0d6e527ffe30fec
Prateek <prateekvermasdg007@gmail.com>
2021-02-08 21:50:57 +0530

    added exception handling logic in the source code while parsing to int.

--- tdd.kta/src/main/java/tdd/kta/StringCalculator.java
+++ tdd.kta/src/main/java/tdd/kta/StringCalculator.java
@@ -1,16 +1,22 @@
 package tdd.kta;
 
+import java.io.Console;
+
 public class StringCalculator {
 	
 	public int Add(String numbers) {
 		int sum=0;
-		if(numbers.isEmpty())
-			sum=0;
-		else {
-			String[] num = numbers.split(",");
-			for(String numString : num) {
-				sum=sum + Integer.parseInt(numString);
+		try {
+			if(numbers.isEmpty())
+				sum=0;
+			else {
+				String[] num = numbers.split(",");
+				for(String numString : num) {
+					sum=sum + Integer.parseInt(numString);
+				}
 			}
+		} catch (Exception e) {
+			System.err.println(e);
 		}
 		return sum;
 	}
