bede7e78a2cc7fa637c1f6c638b035c7b359ca84
Prateek <prateekvermasdg007@gmail.com>
2021-02-08 21:24:23 +0530

    wrote the Add method without implementing the logic and wrote the test case. Here test case fails.

--- tdd.kta/src/main/java/tdd/kta/StringCalculator.java
+++ tdd.kta/src/main/java/tdd/kta/StringCalculator.java
@@ -1,5 +1,9 @@
 package tdd.kta;
 
 public class StringCalculator {
+	
+	public int Add(String numbers) {
+		return -1;
+	}
 
 }
--- tdd.kta/src/test/java/tdd/kta/StringCalculatorTest.java
+++ tdd.kta/src/test/java/tdd/kta/StringCalculatorTest.java
@@ -1,8 +1,18 @@
 package tdd.kta;
 
 import org.junit.Test;
+
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
-
+ 
+ private StringCalculator stringCalculator = new StringCalculator();
+ 
+@Test
+ public void testAdd() {
+	 assertEquals(0, stringCalculator.Add(" "));
+ }
+ 
 }
